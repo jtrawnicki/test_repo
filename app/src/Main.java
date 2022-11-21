d2aef4d63591c54a4ce2b7bf8e9886d021b2af8b
Jakub Trawnicki <kubatrawka@gmail.com>
2022-11-21 12:31:54 +0100

    Hello Hello

--- app/src/Main.java
+++ app/src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("123");
+        System.out.println("Hello Hello!");
     }
 }
